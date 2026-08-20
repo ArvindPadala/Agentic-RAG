3454bf69171f661ade7604ff322e502a91b82bc9
ArvindPadala <padalaarvindchary1@gmail.com>
2026-08-20 10:53:45 -0400

    Fix layout and delete broken lint scripts

--- find_fstrings2.py
+++
@@ -1,9 +0,0 @@
-import glob
-
-for filename in glob.glob("**/*.py", recursive=True):
-    with open(filename, "r") as f:
-        lines = f.readlines()
-        for i, line in enumerate(lines):
-            # check if line has 'f"' or "f'" and ends with {if 'f"' in line or "f'" in line:
-                if line.rstrip().endswith("{"):
-                    print(f"{filename}:{i+1}: {line.strip()}")
